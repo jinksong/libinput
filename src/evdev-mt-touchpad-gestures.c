c745be4c9905d1f73546c0dd3a47326bbf215adc
Jin Song <jsong@magicleap.com>
2016-07-04 13:04:33 -0700

    Debounce multi-touch events
    Due to flakiness of touch events, it is prone to be detected as
    pinch gesture as opposed to swipe.
    Debounce three time to make sure it is a pinch gesture.

--- src/evdev-mt-touchpad-gestures.c
+++ src/evdev-mt-touchpad-gestures.c
@@ -336,6 +336,8 @@ tp_gesture_handle_state_unknown(struct tp_dispatch *tp, uint64_t time)
 	int dir1, dir2;
 	int yres = tp->device->abs.absinfo_y->resolution;
 	int vert_distance;
+	static int pinch_debounce = 3;
+	enum tp_gesture_state next_state = GESTURE_STATE_UNKNOWN;
 
 	/* for two-finger gestures, if the fingers stay unmoving for a
 	 * while, assume (slow) scroll */
@@ -366,16 +368,24 @@ tp_gesture_handle_state_unknown(struct tp_dispatch *tp, uint64_t time)
 	if (tp_gesture_same_directions(dir1, dir2)) {
 		if (tp->gesture.finger_count == 2) {
 			tp_gesture_set_scroll_buildup(tp);
-			return GESTURE_STATE_SCROLL;
+			next_state = GESTURE_STATE_SCROLL;
 		} else if (tp->gesture.enabled) {
-			return GESTURE_STATE_SWIPE;
+			next_state = GESTURE_STATE_SWIPE;
 		}
+	} else if (pinch_debounce--) {
+		log_debug (tp_libinput_context(tp), "Pinch debounced %d\n", pinch_debounce);
+		tp->gesture.initial_time = time;
+		first->gesture.initial = first->point;
+		second->gesture.initial = second->point;
 	} else {
 		tp_gesture_init_pinch(tp);
-		return GESTURE_STATE_PINCH;
+		next_state = GESTURE_STATE_PINCH;
 	}
 
-	return GESTURE_STATE_UNKNOWN;
+	if (next_state != GESTURE_STATE_UNKNOWN)
+		pinch_debounce = 3;
+
+	return next_state;
 }
 
 static enum tp_gesture_state
